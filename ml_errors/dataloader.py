d3236c9a2d51da5d71de3a18fc06d3fa9bc90682
SUBHASH <subha11.11raj@gmail.com>
2025-09-20 00:08:39 +0530

    Update dataloader.py

--- ml_errors/dataloader.py
+++ ml_errors/dataloader.py
@@ -7,8 +7,8 @@ class ToyDataset:
         self.data = data
 
     def __len__(self) -> int:
-        # BUG: Wrong length, off-by-one to trigger test failures
-        return len(self.data) + 1
+    # BUG: Wrong length, off-by-one to trigger test failures
+    return len(self.data)  # ← Fixed
 
     def __getitem__(self, idx: int) -> int:
         return self.data[idx]
